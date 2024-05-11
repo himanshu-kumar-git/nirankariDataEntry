84836d5afb6665cafecaa71faedd1f1e9855dbd1
HIMANSHU <115986142+himanshu-kumar-git@users.noreply.github.com>
2024-05-11 13:18:39 +0530

    input padding change

--- src/Pages/DetailForm.jsx
+++ src/Pages/DetailForm.jsx
@@ -1109,7 +1109,7 @@ const DetailhtmlhtmlForm = () => {
                             type="number"
                             id="akNajar"
                             name="akNajar"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="block w-full p-4 ps-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Enter here .. "
                             value={data.akNajar}
                             onChange={handleInput}
@@ -1134,7 +1134,7 @@ const DetailhtmlhtmlForm = () => {
                             type="number"
                             id="hastiDuniya"
                             name="hastiDuniya"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="block w-full p-4 ps-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Enter here .. "
                             value={data.hastiDuniya}
                             onChange={handleInput}
@@ -1160,7 +1160,7 @@ const DetailhtmlhtmlForm = () => {
                             type="number"
                             id="santNirankariPatrika"
                             name="santNirankariPatrika"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                            className="block w-full p-4 ps-3 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="Enter here .. "
                             value={data.santNirankariPatrika}
                             onChange={handleInput}
