9278cfd070303b6f92d9761bbb9753acd6596d66
HIMANSHU <115986142+himanshu-kumar-git@users.noreply.github.com>
2024-05-29 13:26:21 +0530

    button change

--- src/Components/EntryForm/Entry5.jsx
+++ src/Components/EntryForm/Entry5.jsx
@@ -225,14 +225,14 @@ const Entry5 = ({ masterData, handleEntry5 }) => {
                             </tr>
                           ))}
                         </tbody>
-                        <button
-                          type="button"
-                          onClick={handleAddRow}
-                          className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-600 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-14 ml-2 w-[150px]"
-                        >
-                          नई पंक्ति जोड़ें
-                        </button>
                       </table>
+                      <button
+                        type="button"
+                        onClick={handleAddRow}
+                        className="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-600 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 shadow-lg shadow-lime-500/50 dark:shadow-lg dark:shadow-lime-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 mt-6  w-[150px]"
+                      >
+                        नई पंक्ति जोड़ें
+                      </button>
                     </div>
                   </div>
                 )}
